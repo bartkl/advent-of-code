86e6ed60a5705b219c43c09066f7033e56caacd3
Bart Kleijngeld <bart.kleijngeld@alliander.com>
2020-12-02 18:54:49 +0100

    Finished day 1

---
+++ 01/01.py
@@ -0,0 +1,30 @@
+import itertools
+
+
+EXPENSES_FILE = 'expenses.txt'
+
+
+def read_expenses(expenses_filepath):
+    with open(expenses_filepath) as expenses_fp:
+        return [int(expense)
+                for line in expenses_fp
+                if (expense := line.strip())]
+
+
+if __name__ == '__main__':
+    expenses = read_expenses(EXPENSES_FILE)
+
+    #  Get product of pairs with sum 2020.
+    pairs = itertools.combinations(expenses, 2)
+
+    for p, q in pairs:
+        if p + q == 2020:
+            print(p * q)
+
+
+    # Get product of triples with sum 2020.
+    triples = itertools.combinations(expenses, 3)
+
+    for p, q, r in triples:
+        if p + q  + r== 2020:
+            print(p * q * r)
