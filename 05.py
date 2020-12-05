6c2d296dd01d476dab629156b751b1ff9d7ecaec
Bart Kleijngeld <bart.kleijngeld@alliander.com>
2020-12-05 16:16:15 +0100

    Tidied up day 5 even further

--- 05.py
+++ 05.py
@@ -1,16 +1,6 @@
 from itertools import chain
 
 
-BOARDING_PASSES_FILE = 'data/05/boarding_passes.txt'
-
-
-def read_boarding_passes(passes_file):
-    with open(passes_file) as f:
-        seats = {Seat(specifier.strip()) for specifier in f}
-
-    return seats
-
-
 class Seat(str):
     @property
     def id(self) -> int:
@@ -29,24 +19,25 @@ class Seat(str):
 
 
 if __name__ == '__main__':
-    seats = read_boarding_passes(BOARDING_PASSES_FILE)
+    with open('data/05/boarding_passes.txt') as f:
+        seats = {Seat(specifier.strip()) for specifier in f}
 
     # Get max boarding pass ID.
     max_seat_id = max(s.id for s in seats)
-    print(max_seat_id)
+    print(f"Highest seat ID in list: {max_seat_id}.")
 
     # Determine your seat ID.
     all_seat_ids = set(range(2**10))
     missing_seat_ids = (
         all_seat_ids
-        - {s.id for s in seats}
+        - {s.id for s in seats}  # Taken seats.
         - {s_id for s_id in chain(range(2**3),
-                                  range(2**10 - 8, 2**10))}
+                                  range(2**10 - 8, 2**10))}  # Lacking first and last rows.
     )
 
     # The seats with IDs +1 and -1 will be in the list, so
-    # in the missing seats list they will _not_ occur.
+    # in the missing seats list they will not occur.
     for seat_id in missing_seat_ids:
         if not {seat_id - 1, seat_id + 1} & missing_seat_ids:
-            print(seat_id)
+            print(f"My seat is the one with ID: {seat_id}.")
             exit()
