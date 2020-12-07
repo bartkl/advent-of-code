87fb7e8fdc8b51cf333008e287f7fe1b18805ee5
Bart Kleijngeld <bart.kleijngeld@alliander.com>
2020-12-07 18:13:09 +0100

    Finished day 7 (part 1)

--- 07.py
+++ 07.py
@@ -1,4 +1,5 @@
 from collections import UserDict
+import time
 
 
 class RuleTree(UserDict):
@@ -12,15 +13,42 @@ class RuleTree(UserDict):
 
                 key, values = rule.split(' contain ')
 
-                self[key] = []
+                self[key] = {}
                 if values == 'no other bags.':
                     continue
                 else:
                     values = values.rstrip('.').split(', ')
                     for value in values:
                         amount, bag = value.split(' ', maxsplit=1)
-                        self[key].append((int(amount), bag))
+                        if (amount := int(amount)) == 1:
+                            # Normalize to plural form.
+                            bag += 's'
+                        self[key][bag] = amount
         return self
+
+    def _suited_bags_for(self, bags):
+        if not bags:
+            return set()
+
+        containers = set()
+        for bag in bags:
+            for container_name, container in self.items():
+                if bag in container.keys():
+                    containers.add(container_name)
+
+        return containers | self._suited_bags_for(containers)
+
+    def suited_bags_for(self, bag):
+        suited_bags = self._suited_bags_for({bag})
+        return suited_bags
+
+
+
 
 if __name__ == '__main__':
     ruletree = RuleTree.from_file('data/07/rules.txt')
+    suited_bags = ruletree.suited_bags_for('shiny gold bags')
+
+    print(suited_bags)
+    print(len(suited_bags))
+
