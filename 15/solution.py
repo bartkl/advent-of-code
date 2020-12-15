e9c0e6006373c32f1078b7a6cc593f730f273668
Bart Kleijngeld <bartkl@gmail.com>
2020-12-15 21:35:40 +0100

    Day 15: solved

--- 15/solution.py
+++ 15/solution.py
@@ -1,25 +1,27 @@
-from array import array
-
-
 def numbers(start_list):
-    cache = array('l', [])
+    cache = {}
 
-    for val in start_list:
-        cache.insert(0, val)
+    for i, val in enumerate(start_list, 1):
+        cache[val] = i
         yield val
+    del cache[val]
+    i += 1
 
     while True:
-        for d, val in enumerate(cache[1:], 1):
-            if val == cache[0]:
-                cache.insert(0, d)
-                yield d
-                break
-        else:
-            cache.insert(0, 0)
-            yield 0
+        prev_val = val
+        try:
+            val = (i - 1) - cache[val]
+        except KeyError:
+            val = 0
+        finally:
+            cache[prev_val] = i - 1
+            # print(cache)
+            # input()
+        yield val
+        i += 1
 
 
-numbers_gen = numbers([0,20,7,16,1,18,15])
+numbers_gen = numbers([0, 20, 7, 16, 1, 18, 15])
 
 for i, number in enumerate(numbers_gen, 1):
     if i == 30000000:
