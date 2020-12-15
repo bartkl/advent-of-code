a96cb9c8bdb91a2691143cdc19b62f47d3b67b8c
Bart Kleijngeld <bartkl@gmail.com>
2020-12-15 20:55:29 +0100

    Day 15: Solved part 1 with efficient array instead of list, and without need to reverse

--- 15/solution.py
+++ 15/solution.py
@@ -2,27 +2,26 @@ from array import array
 
 
 def numbers(start_list):
-    cache = []
-    for i, val in enumerate(start_list, 1):
-        cache.append(val)
+    cache = array('l', [])
+
+    for val in start_list:
+        cache.insert(0, val)
         yield val
 
     while True:
-        for d, val in enumerate(cache[-2::-1], 1):
-            if val == cache[-1]:
-                cache.append(d)
+        for d, val in enumerate(cache[1:], 1):
+            if val == cache[0]:
+                cache.insert(0, d)
                 yield d
                 break
         else:
-            cache.append(0)
-            yield cache[-1]
-
-        i += 1
+            cache.insert(0, 0)
+            yield 0
 
 
 numbers_gen = numbers([0,20,7,16,1,18,15])
 
 for i, number in enumerate(numbers_gen, 1):
-    print(f'Number {i} = {number}')
-    if i == 2020:
+    if i == 30000000:
+        print(f'Number {i} = {number}')
         exit()
