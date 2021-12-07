f894c1dd502656d7f55f1d41d928147bd7bd0add
Bart Kleijngeld <bartkl@gmail.com>
2021-12-07 17:38:40 +0100

    Day 06 improved

--- 06/solution.py
+++ 06/solution.py
@@ -1,33 +1,37 @@
-from collections import Counter
+from collections import deque, Counter
 from pathlib import Path
-from more_itertools import nth
-
-INPUT = Path("input.txt")
+from more_itertools import nth, consume
 
 
-def read_initial_state(text_file):
-    text = text_file.read_text()
-    return Counter(int(internal_timer) for internal_timer in text.split(","))
+def read_puzzle_input(text_file: Path):
+    return [int(internal_timer) for internal_timer in text_file.read_text().split(",")]
 
 
-def decrement_counter_keys(c):
-    for key in sorted(c.keys()):
-        c[key - 1] = c.pop(key)
+def count_fish_by_internal_timer(state):
+    counts = Counter(state)
+
+    return deque(counts.get(internal_timer, 0) for internal_timer in range(9))
 
 
-def evolve(population):
+def evolve_fish_population_counts(counts):
+    yield counts  # Day 0 counts on initialization.
     while True:
-        decrement_counter_keys(population)
-        if -1 in population.keys():
-            population.update({6: population[-1]})
-            population.update({8: population[-1]})
-            del population[-1]
+        counts.rotate(-1)
+        if counts[8] > 0:
+            counts[6] += counts[8]
 
-        yield population
+        yield counts
 
 
 if __name__ == "__main__":
-    init_state = read_initial_state(INPUT)
-    e = evolve(init_state)
-    x = nth(e, 255)
-    print(sum(x.values()))
+    initial_internal_timers = read_puzzle_input(Path("input.txt"))
+    initial_counts = count_fish_by_internal_timer(initial_internal_timers)
+    fish_population_counts_evolution = evolve_fish_population_counts(initial_counts)
+
+    # Question 1, for 80 days.
+    # total_fish_count_after_80_days = sum(nth(fish_population_counts_evolution, 80))
+    # print(total_fish_count_after_80_days)
+
+    # Question 2, for 256 days.
+    total_fish_count_after_80_days = sum(nth(fish_population_counts_evolution, 256))
+    print(total_fish_count_after_80_days)
